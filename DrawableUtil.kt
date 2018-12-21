ea49ce2818031cc942f26fd431f3da0fd820d175
Ivan Kataev <aslanisl@mail.ru>
2018-12-21 10:47:07 +0300

    Update DrawableUtil.kt

--- DrawableUtil.kt
+++ DrawableUtil.kt
@@ -109,85 +109,6 @@ object DrawableUtil {
         backgroundColor: Int
     ): Drawable {
         val shadowColor: Int = context.getColorCompatAttr(R.attr.colorShadow)
-        val maxShadowAlpha = 30
-
-        val layers = mutableListOf<Drawable>()
-        // We just hardcore shadow layers count. Can change it if need
-        val shadowCount = 5
-        val shadowAlphaStep = maxShadowAlpha / shadowCount
-
-        val leftShadowPaddingStep = padding.left.toFloat() / shadowCount
-        val topShadowPaddingStep = padding.top.toFloat() / shadowCount
-        val rightShadowPaddingStep = padding.right.toFloat() / shadowCount
-        val bottomShadowPaddingStep = padding.bottom.toFloat() / shadowCount
-        for (i in 0 until shadowCount) {
-            val layerPadding = Rect()
-            // Don't add padding to first layer
-            if (i > 0) {
-                layerPadding.left = leftShadowPaddingStep.roundToInt()
-                layerPadding.top = topShadowPaddingStep.roundToInt()
-                layerPadding.right = rightShadowPaddingStep.roundToInt()
-                layerPadding.bottom = bottomShadowPaddingStep.roundToInt()
-            }
-
-            layers += getRoundedShadowLayer(
-                shadowColor,
-                i * shadowAlphaStep,
-                false, leftTopCorner, rightTopCorner, rightBottomCorner, leftBottomCorner, radiusCorner, layerPadding)
-        }
-
-        layers += getRoundedShadowLayer(backgroundColor, 255, true, leftTopCorner, rightTopCorner, rightBottomCorner, leftBottomCorner, radiusCorner, padding)
-
-        return LayerDrawable(layers.toTypedArray())
-    }
-
-    private fun getRoundedShadowLayer(
-        @ColorInt color: Int,
-        @IntRange(from = 0L, to = 255L) alpha: Int = 255,
-        removePadding: Boolean = false,
-        leftTopCorner: Boolean,
-        rightTopCorner: Boolean,
-        rightBottomCorner: Boolean,
-        leftBottomCorner: Boolean,
-        radiusCorner: Float,
-        padding: Rect
-    ): Drawable {
-
-        val drawable = GradientDrawable()
-        val radii = FloatArray(8)
-        radii[0] = if (leftTopCorner) radiusCorner else 0f
-        radii[1] = if (leftTopCorner) radiusCorner else 0f
-
-        radii[2] = if (rightTopCorner) radiusCorner else 0f
-        radii[3] = if (rightTopCorner) radiusCorner else 0f
-
-        radii[4] = if (rightBottomCorner) radiusCorner else 0f
-        radii[5] = if (rightBottomCorner) radiusCorner else 0f
-
-        radii[6] = if (leftBottomCorner) radiusCorner else 0f
-        radii[7] = if (leftBottomCorner) radiusCorner else 0f
-
-        drawable.cornerRadii = radii
-
-        val shadowPadding = Rect(padding)
-        if (removePadding) shadowPadding.set(0, 0, 0, 0)
-        drawable.setPrivateField("mPadding", shadowPadding)
-        drawable.setColor(color)
-        drawable.alpha = alpha
-        return drawable
-    }
-
-    fun getRoundedShadowBackground(
-        context: Context,
-        leftTopCorner: Boolean,
-        rightTopCorner: Boolean,
-        rightBottomCorner: Boolean,
-        leftBottomCorner: Boolean,
-        radiusCorner: Float,
-        padding: Rect,
-        backgroundColor: Int
-    ): Drawable {
-        val shadowColor: Int = context.getColorCompatAttr(R.attr.colorShadow)
 
         return ShapeDrawable().apply {
             shape = RoundedShadow(
@@ -197,12 +118,13 @@ object DrawableUtil {
                 rightTopCorner,
                 rightBottomCorner,
                 leftBottomCorner,
-                radiusCorner,
-                padding
+                radiusCorner
             )
             setPadding(padding.left, padding.top, padding.right, padding.bottom)
         }
     }
+
+    private const val COUNT_LAYERS = 5
 
     internal class RoundedShadow constructor(
         @ColorInt private val backgroundColor: Int,
@@ -211,59 +133,61 @@ object DrawableUtil {
         private val rightTopCorner: Boolean,
         private val rightBottomCorner: Boolean,
         private val leftBottomCorner: Boolean,
-        private val radiusCorner: Float,
-        private val padding: Rect
+        private val radiusCorner: Float
     ) : RectShape() {
 
-        private var radialGradient: RadialGradient? = null
-        private val backgroundPaint: Paint = Paint().apply { color = backgroundColor }
         private val shadowPaint: Paint = Paint().apply {
             isAntiAlias = true
-            style = Style.FILL
-            strokeJoin = Paint.Join.ROUND
-            strokeCap = Paint.Cap.ROUND
-            strokeWidth = shadowPadding.toFloat()
+            style = Style.STROKE
         }
 
-        private lateinit var shadowRect: Path
-        private lateinit var backgroundRect: Path
+        private val backgroundPaint = Paint().apply {
+            style = Style.FILL
+            color = backgroundColor
+        }
+
+        private val colors = IntArray(COUNT_LAYERS) { position ->
+            ColorUtils.setAlphaComponent(shadowColor, 5 * position)
+        }
+
+        private val paths = mutableListOf<Path>()
+        private val backgroundPath = Path()
+
+        private val shadowRadius
+            get() = shadowPadding
 
         init {
-            updateRadialGradient(rect().width().toInt())
-            updateRoundedRect()
+            updatePaths()
         }
 
         override fun onResize(width: Float, height: Float) {
             super.onResize(width, height)
-            updateRadialGradient(width.toInt())
-            updateRoundedRect()
+            updatePaths()
         }
 
         override fun draw(canvas: Canvas, paint: Paint) {
-            canvas.drawPath(shadowRect, shadowPaint)
-            canvas.drawPath(backgroundRect, backgroundPaint)
+            paths.forEachIndexed { index, path ->
+                shadowPaint.color = colors[index]
+                canvas.drawPath(path, shadowPaint)
+            }
+            canvas.drawPath(backgroundPath, backgroundPaint)
         }
 
-        private fun updateRadialGradient(diameter: Int) {
-            val radius = diameter / 2f
-            if (radius < 1) return
-//            shadowPaint.maskFilter = BlurMaskFilter(diameter.toFloat(), Blur.NORMAL)
-
-            val center = diameter / 2f
-//            radialGradient = RadialGradient(center, center, radius, intArrayOf(shadowColor, Color.TRANSPARENT), null, CLAMP)
-//            shadowPaint.shader = radialGradient
-        }
-
-        private fun updateRoundedRect() {
+        private fun updatePaths() {
             val width = rect().width()
             val height = rect().height()
 
-            shadowRect = getRoundedRect(0f, 0f, width, height, radiusCorner, radiusCorner, leftTopCorner, rightTopCorner, rightBottomCorner, leftBottomCorner)
-            backgroundRect = getRoundedRect(
-                padding.left.toFloat(),
-                padding.top.toFloat(),
-                width - padding.right,
-                height - padding.bottom,
+            paths.clear()
+            val shadowStep = shadowRadius.toFloat() / COUNT_LAYERS
+            for (i in 0 until COUNT_LAYERS) {
+                val rectF = RectF(0f + shadowStep * i, 0f + shadowStep * i, width - shadowStep * i, height - shadowStep * i)
+                val path = CanvasUtils.getRoundedRectPath(rectF, radiusCorner, radiusCorner, leftTopCorner, rightTopCorner, rightBottomCorner, leftBottomCorner)
+                paths.add(path)
+            }
+            shadowPaint.strokeWidth = shadowStep
+
+            val backgroundPath = CanvasUtils.getRoundedRectPath(
+                RectF(shadowRadius.toFloat(), shadowRadius.toFloat(), width - shadowRadius, height - shadowRadius),
                 radiusCorner,
                 radiusCorner,
                 leftTopCorner,
@@ -271,55 +195,7 @@ object DrawableUtil {
                 rightBottomCorner,
                 leftBottomCorner
             )
-        }
-
-        private fun getRoundedRect(left: Float, top: Float, right: Float, bottom: Float, rxRadius: Float, ryRadius: Float, tl: Boolean, tr: Boolean, br: Boolean, bl: Boolean): Path {
-            var rx = rxRadius
-            var ry = ryRadius
-            val path = Path()
-            if (rx < 0) rx = 0f
-            if (ry < 0) ry = 0f
-            val width = right - left
-            val height = bottom - top
-            if (rx > width / 2) rx = width / 2
-            if (ry > height / 2) ry = height / 2
-            val widthMinusCorners = width - 2 * rx
-            val heightMinusCorners = height - 2 * ry
-
-            path.moveTo(right, top + ry)
-            if (tr)
-                path.rQuadTo(0f, -ry, -rx, -ry)//top-right corner
-            else {
-                path.rLineTo(0f, -ry)
-                path.rLineTo(-rx, 0f)
-            }
-            path.rLineTo(-widthMinusCorners, 0f)
-            if (tl)
-                path.rQuadTo(-rx, 0f, -rx, ry) //top-left corner
-            else {
-                path.rLineTo(-rx, 0f)
-                path.rLineTo(0f, ry)
-            }
-            path.rLineTo(0f, heightMinusCorners)
-
-            if (bl)
-                path.rQuadTo(0f, ry, rx, ry)//bottom-left corner
-            else {
-                path.rLineTo(0f, ry)
-                path.rLineTo(rx, 0f)
-            }
-
-            path.rLineTo(widthMinusCorners, 0f)
-            if (br)
-                path.rQuadTo(rx, 0f, rx, -ry) //bottom-right corner
-            else {
-                path.rLineTo(rx, 0f)
-                path.rLineTo(0f, -ry)
-            }
-
-            path.rLineTo(0f, -heightMinusCorners)
-            path.close()
-            return path
+            this.backgroundPath.set(backgroundPath)
         }
     }
 
